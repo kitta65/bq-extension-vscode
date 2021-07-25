7ae54b939f2398cbf63d2eb2e033bc11bfcffb43
dr666m1 <skndr666m1@gmail.com>
2021-07-25 23:54:34 +0900

    minimum dry run

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,11 +7,11 @@ import {
   TextDocumentSyncKind,
   InitializeResult,
 } from "vscode-languageserver/node";
-
+import { BigQuery } from "@google-cloud/bigquery";
 import { TextDocument } from "vscode-languageserver-textdocument";
 
 const connection = createConnection(ProposedFeatures.all);
-
+const bqClient = new BigQuery();
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 connection.onInitialize(() => {
@@ -25,6 +25,10 @@ connection.onInitialize(() => {
 
 documents.onDidChangeContent((change) => {
   validateTextDocument(change.document);
+});
+
+documents.onDidSave((change) => {
+  dryRun(change.document);
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
@@ -47,6 +51,51 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
     diagnostics.push(diagnostic);
   }
   connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+async function dryRun(textDocument: TextDocument): Promise<void> {
+  const text = textDocument.getText();
+  let diagnostic: Diagnostic;
+  try {
+    // NOTE bqClient.createQueryJob()[1].statistics.totalBytesProcessed may be useful
+    const [_, apiResponse] = await bqClient.createQueryJob({
+      query: text,
+      dryRun: true,
+    });
+    if (apiResponse.statistics && apiResponse.statistics.totalBytesProcessed) {
+      diagnostic = {
+        severity: DiagnosticSeverity.Information,
+        range: {
+          start: textDocument.positionAt(0),
+          end: textDocument.positionAt(1),
+        },
+        message: apiResponse.statistics.totalBytesProcessed,
+      };
+    } else {
+      diagnostic = {
+        severity: DiagnosticSeverity.Error,
+        range: {
+          start: textDocument.positionAt(0),
+          end: textDocument.positionAt(1),
+        },
+        message: "Could not receive information about total bytes processed.",
+      };
+    }
+  } catch (e) {
+    // TODO detect `not log in` error
+    diagnostic = {
+      severity: DiagnosticSeverity.Information,
+      range: {
+        start: textDocument.positionAt(0),
+        end: textDocument.positionAt(1),
+      },
+      message: e.message, // Syntax error: Unexpected end of script at [1:7]
+    };
+  }
+  connection.sendDiagnostics({
+    uri: textDocument.uri,
+    diagnostics: [diagnostic],
+  });
 }
 
 documents.listen(connection);
