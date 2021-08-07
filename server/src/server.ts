bf2173bb0ce458641284808146a610dcc2177be5
dr666m1 <skndr666m1@gmail.com>
2021-08-07 18:02:51 +0900

    improve error hilight position

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import {
 } from "vscode-languageserver/node";
 import { BigQuery } from "@google-cloud/bigquery";
 import { TextDocument } from "vscode-languageserver-textdocument";
+import { tokenize } from "@dr666m1/bq2cst";
 
 const connection = createConnection(ProposedFeatures.all);
 const bqClient = new BigQuery();
@@ -41,22 +42,36 @@ async function dryRun(textDocument: TextDocument): Promise<void> {
     } else {
       msg = "???B";
     }
+    connection.sendDiagnostics({
+      uri: textDocument.uri,
+      diagnostics: [],
+    }); // clear old diagnostics
     connection.sendNotification("dryRun", msg);
   } catch (e) {
     // TODO detect `not log in` error
-    diagnostic = {
-      severity: DiagnosticSeverity.Information,
-      range: {
-        start: textDocument.positionAt(0),
-        end: textDocument.positionAt(1),
-      },
-      message: e.message, // Syntax error: Unexpected end of script at [1:7]
-    };
+    const msg = e.message; // Syntax error: Unexpected end of script at [1:7]
+    const matchResult = msg.match(/\[([0-9]+):([0-9]+)\]/);
+    if (matchResult) {
+      const token = getTokenByRowColumn(
+        text,
+        Number(matchResult[1]),
+        Number(matchResult[2])
+      );
+      const position = getPositionByRowColumn(text, token.line, token.column);
+      diagnostic = {
+        severity: DiagnosticSeverity.Error,
+        range: {
+          start: textDocument.positionAt(position),
+          end: textDocument.positionAt(position + token.literal.length),
+        },
+        message: msg,
+      };
+      connection.sendDiagnostics({
+        uri: textDocument.uri,
+        diagnostics: [diagnostic],
+      });
+    }
     connection.sendNotification("dryRun", "ERROR");
-    connection.sendDiagnostics({
-      uri: textDocument.uri,
-      diagnostics: [diagnostic],
-    });
   }
 }
 
@@ -77,4 +92,32 @@ function formatBytes(bytes: number) {
   } else {
     return `${(bytes / 1024 ** 6).toFixed(1)}PB`;
   }
+}
+
+function getPositionByRowColumn(text: string, row: number, column: number) {
+  const rowLengthArr = text.split("\n").map((x) => x.length + 1); // add length of "\n"
+  const position =
+    rowLengthArr.slice(0, row - 1).reduce((x, y) => x + y, 0) + column;
+  return position - 1;
+}
+
+function getTokenByRowColumn(text: string, row: number, column: number) {
+  const errorPosition = getPositionByRowColumn(text, row, column);
+  const tokens = tokenize(text);
+  let prevTokenPosition = 0;
+  for (const token of tokens) {
+    const currTokenPosition = getPositionByRowColumn(
+      text,
+      token.line,
+      token.column
+    );
+    if (
+      prevTokenPosition <= errorPosition &&
+      errorPosition <= currTokenPosition
+    ) {
+      return token;
+    }
+    prevTokenPosition = currTokenPosition;
+  }
+  return tokens[tokens.length - 1];
 }
