bd8a060e52f132723dcd3f6df100dbbe2856b1fc
dr666m1 <skndr666m1@gmail.com>
2021-09-03 23:50:47 +0900

    add dummy test

---
+++ client/src/test/cache.test.ts
@@ -0,0 +1,5 @@
+import * as assert from "assert";
+
+it("tests", function () {
+  assert.strictEqual(0, 1);
+});
--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -1,1 +1,30 @@
-throw new Error("no test specified!!");
+import * as path from "path";
+import * as Mocha from "mocha";
+import * as glob from "glob";
+
+// NOTE If the function is not named `run`, you'll get an error.
+export function run(): Promise<void> {
+  const mocha = new Mocha({ ui: "bdd", color: true });
+  mocha.timeout(100 * 1000);
+  const testsRoot = __dirname;
+  return new Promise<void>((resolve, reject) => {
+    glob("**.test.js", { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return reject(err);
+      }
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+      try {
+        mocha.run((failures) => {
+          if (0 < failures) {
+            reject(new Error(`${failures} tests failed.`));
+          } else {
+            resolve();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
+  });
+}
