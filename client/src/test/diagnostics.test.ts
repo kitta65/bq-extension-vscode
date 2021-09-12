e45e244bcd8468ba082fdcfec2e548dc69e9cfc7
dr666m1 <skndr666m1@gmail.com>
2021-09-12 20:39:07 +0900

    Add tests related to 5dc2047a8d45dd957e8da9c61f727f8b151632ee

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -2,6 +2,11 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 import * as util from "./util";
 
+/**NOTE
+ * The position of the diagnostic is not tested here
+ * because it depends on the message from BigQuery (or `@dr666m1/bq2cst`).
+ */
+
 describe("Diagnostics", function () {
   beforeEach(async function () {
     await util.createTextDocument("diagnostics.bq");
@@ -9,15 +14,27 @@ describe("Diagnostics", function () {
   afterEach(async function () {
     await util.deleteTextDocument("diagnostics.bq");
   });
-  it("success", async function () {
+  it("dry-run-success", async function () {
     await util.insert("diagnostics.bq", new vscode.Position(0, 0), "SELECT 1;");
     await vscode.commands.executeCommand("bqExtensionVSCode.dryRun")
     const diagnostics = vscode.languages.getDiagnostics(util.getDocUri("diagnostics.bq"))
     assert.strictEqual(diagnostics.length, 0)
   });
-  it("fail", async function () {
+  it("dry-run-fail", async function () {
     await util.insert("diagnostics.bq", new vscode.Position(0, 0), "SELECT 1;;");
     await vscode.commands.executeCommand("bqExtensionVSCode.dryRun")
+    const diagnostics = vscode.languages.getDiagnostics(util.getDocUri("diagnostics.bq"))
+    assert.strictEqual(diagnostics.length, 1)
+  });
+  it("bq2cst-success", async function () {
+    await util.insert("diagnostics.bq", new vscode.Position(0, 0), "SELECT ''");
+    await util.sleep(1 * 1000) // wait for processing
+    const diagnostics = vscode.languages.getDiagnostics(util.getDocUri("diagnostics.bq"))
+    assert.strictEqual(diagnostics.length, 0)
+  });
+  it("bq2cst-fail", async function () {
+    await util.insert("diagnostics.bq", new vscode.Position(0, 0), "SELECT '");
+    await util.sleep(1 * 1000) // wait for processing
     const diagnostics = vscode.languages.getDiagnostics(util.getDocUri("diagnostics.bq"))
     assert.strictEqual(diagnostics.length, 1)
   });
