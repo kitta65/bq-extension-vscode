8260ccde4b5a6bfc702cd2177b065ca47406fbd1
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-03-16 11:19:21 +0900

    add format test

---
+++ client/src/test/suite/format.test.ts
@@ -0,0 +1,26 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as util from "./util";
+
+const filename = "format.bq";
+
+describe("Format", function () {
+  beforeEach(async function () {
+    await util.createTextDocument(filename);
+  });
+  afterEach(async function () {
+    await util.deleteTextDocument(filename);
+  });
+  it("format", async function () {
+    const sql = "SELECT 1    ;";
+    util.insert(filename, new vscode.Position(0, 0), sql);
+    await vscode.commands.executeCommand("editor.action.formatDocument");
+    const texts = vscode.workspace.textDocuments.filter(
+      (text) => text.fileName === filename,
+    );
+    assert.equal(texts.length, 1);
+    const text = texts[0];
+    const actual = text.getText();
+    assert.equal(actual, "SELECT 1;");
+  });
+});
