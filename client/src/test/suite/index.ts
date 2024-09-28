65009192db9dbf1aab68cec0a04b1b03b8d8dd1e
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-09-28 13:09:18 +0900

    install glob explicitly

--- client/src/test/suite/index.ts
+++ client/src/test/suite/index.ts
@@ -1,35 +1,23 @@
 import * as path from "path";
 import * as Mocha from "mocha";
-import * as glob from "glob";
+import { globSync } from "glob";
 import * as vscode from "vscode";
 import * as util from "./util";
 
 // NOTE If the function is not named `run`, you'll get an error.
-export function run(): Promise<void> {
+export function run() {
   const mocha = new Mocha({ ui: "bdd", color: true });
   mocha.timeout(0);
   mocha.globalSetup(globalSetup);
   const testsRoot = __dirname;
-  console.log(glob);
-  return new Promise<void>((resolve, reject) => {
-    glob("**.test.js", { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return reject(err);
-      }
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-      try {
-        mocha.run((failures) => {
-          if (0 < failures) {
-            reject(new Error(`${failures} tests failed.`));
-          } else {
-            resolve();
-          }
-        });
-      } catch (err) {
-        console.error(err);
-        reject(err);
-      }
-    });
+  globSync("**.test.js", { cwd: testsRoot }).forEach((f) => {
+    mocha.addFile(path.resolve(testsRoot, f));
+  });
+
+  mocha.run((failures) => {
+    if (0 < failures) {
+      throw `${failures} tests failed.`;
+    }
   });
 }
 
