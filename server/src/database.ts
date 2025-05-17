74da23c8f1d57f19bc926fad619e2b83c6ca5ed3
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-05-17 15:05:23 +0900

    uninstall dblite

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,15 +1,8 @@
-import dblite from "dblite";
 import * as fs from "fs";
 import { BigQuery } from "@google-cloud/bigquery";
 import { exec } from "child_process";
 import { dirname } from "path";
 import Datastore from "@seald-io/nedb";
-
-type DatasetRecord = {
-  project: string;
-  dataset: string;
-  location: string;
-};
 
 type Document = {
   project: string;
@@ -19,49 +12,19 @@ type Document = {
   columns?: { column: string; data_type: string }[];
 };
 
-const createTableProjects = `
-CREATE TABLE IF NOT EXISTS projects (
-  project TEXT,
-  PRIMARY KEY (project)
-);`;
-const createTableDatasets = `
-CREATE TABLE IF NOT EXISTS datasets (
-  project TEXT,
-  dataset TEXT,
-  location TEXT,
-  PRIMARY KEY (project, dataset)
-);`;
-const createTableColumns = `
-CREATE TABLE IF NOT EXISTS columns (
-  project TEXT,
-  dataset TEXT,
-  table_name TEXT,
-  column TEXT,
-  data_type TEXT,
-  PRIMARY KEY (project, dataset, table_name, column)
-);
-`;
-
 export class CacheDB {
-  public static async initialize(filename: string, nedbFileName: string) {
-    await fs.promises.mkdir(dirname(filename), { recursive: true });
-    const db = new CacheDB(filename, nedbFileName);
-    await Promise.all([
-      db.query(createTableProjects),
-      db.query(createTableDatasets),
-      db.query(createTableColumns),
-      db.nedb.autoloadPromise,
-    ]);
+  public static async initialize(nedbFileName: string) {
+    await fs.promises.mkdir(dirname(nedbFileName), { recursive: true });
+    const db = new CacheDB(nedbFileName);
+    await Promise.all([db.nedb.autoloadPromise]);
     return db;
   }
 
-  private db: SQLite;
   private bqClient = new BigQuery();
   public nedb: Datastore<Document>;
   private nedbFileName: string;
 
-  private constructor(filename: string, nedbFileName: string) {
-    this.db = dblite(filename);
+  private constructor(nedbFileName: string) {
     this.nedbFileName = nedbFileName;
     this.nedb = new Datastore({ filename: nedbFileName, autoload: true });
   }
@@ -70,35 +33,6 @@ export class CacheDB {
     await this.nedb.dropDatabaseAsync();
     this.nedb = new Datastore({ filename: this.nedbFileName, autoload: true });
     await this.nedb.autoloadPromise;
-  }
-
-  public query(sql: string, paramsOrFields?: any[]): Promise<any[]>;
-  public query(sql: string, params?: any[], fields?: any[]): Promise<any[]>;
-  public query(sql: string, arg1?: any[], arg2?: any[]): Promise<any[]> {
-    return new Promise((resolve, reject) => {
-      if (arg2) {
-        this.db.query(sql, arg1, arg2, (err: any, data: any[] | undefined) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(data || []);
-        });
-      } else if (arg1) {
-        this.db.query(sql, arg1, (err: any, data: any[] | undefined) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(data || []);
-        });
-      } else {
-        this.db.query(sql, (err: any, data: any[] | undefined) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(data || []);
-        });
-      }
-    });
   }
 
   private getAvailableProjects() {
@@ -121,7 +55,13 @@ export class CacheDB {
   }
 
   private getAvailableDatasets(project: string) {
-    return new Promise<DatasetRecord[]>((resolve, reject) => {
+    return new Promise<
+      {
+        project: string;
+        dataset: string;
+        location: string;
+      }[]
+    >((resolve, reject) => {
       exec(
         `bq ls --datasets=true --project_id='${project}' --format=json --max_results=1000`,
         (_, stdout) => {
@@ -223,60 +163,8 @@ LIMIT 10000;`,
   }
 
   public async updateCacheForTest(_: string[]) {
+    // TODO
     const insertQueries: Promise<any>[] = [];
-
-    // US
-    const project = "bq-extension-vscode";
-    const dataset = "bq_extension_vscode_test";
-    insertQueries.push(
-      this.query(`INSERT OR IGNORE INTO projects (project) VALUES (?);`, [
-        project,
-      ]),
-    );
-    insertQueries.push(
-      this.query(
-        `INSERT OR IGNORE INTO datasets (project, dataset, location) VALUES (?, ?, ?);`,
-        [project, dataset, "US"],
-      ),
-    );
-    for (const table of ["t", "u_*"]) {
-      const columns: { column: string; data_type: string }[] = [
-        { column: "str", data_type: "STRING" },
-        { column: "int", data_type: "INT64" },
-        { column: "float", data_type: "FLOAT64" },
-        { column: "bool", data_type: "BOOLEAN" },
-        { column: "arr", data_type: "ARRAY<INT64>" },
-        {
-          column: "nested",
-          data_type:
-            "STRUCT<arr2 ARRAY<INT64>, str2 STRING, int2 INT64, nested2 STRUCT<nested3 STRUCT<str4 STRING>, int3 INT64>>",
-        },
-      ];
-      columns.forEach((c) =>
-        insertQueries.push(
-          this.query(
-            "INSERT OR IGNORE INTO columns (project, dataset, table_name, column, data_type) VALUES (?, ?, ?, ?, ?);",
-            [project, dataset, table, c.column, c.data_type],
-          ),
-        ),
-      );
-    }
-
-    // asia-northeast1
-    const datasetAsia = "bq_extension_vscode_test_asia";
-    insertQueries.push(
-      this.query(
-        `INSERT OR IGNORE INTO datasets (project, dataset, location) VALUES (?, ?, ?);`,
-        [project, datasetAsia, "asia-northeast1"],
-      ),
-    );
-    insertQueries.push(
-      this.query(
-        "INSERT OR IGNORE INTO columns (project, dataset, table_name, column, data_type) VALUES (?, ?, ?, ?, ?);",
-        [project, datasetAsia, "v", "str", "STRING"],
-      ),
-    );
-
     await Promise.all(insertQueries);
   }
 }
